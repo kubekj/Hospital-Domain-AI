8c0f1b83abc450d8e863e6b1fdcf7674414912f9
kubekj <kuba.jedraszka@gmail.com>
2024-04-02 14:49:00 +0200

    Small base heurisitc

--- src/heuristics/heuristic.py
+++ src/heuristics/heuristic.py
@@ -3,45 +3,30 @@ from abc import ABCMeta, abstractmethod
 from src.domain.atom import AgentAt, BoxAt, Free
 from src.domain.state import State
 
-NO_CHOKE_POINT = -1
+# Goal count as a default heuristic
+def extract_goal_positions(atom_type, initial_state, item_attr):
+    return {getattr(lit, item_attr): lit.loc for lit in initial_state.goal_literals if isinstance(lit, atom_type)}
+
+
+def count_goals_not_met(state, goal_positions, atom_type, item_attr):
+    return sum(1 for item, goal_loc in goal_positions.items()
+               if any(isinstance(lit, atom_type)
+                      and getattr(lit, item_attr) == item
+                      and lit.loc != goal_loc for lit in state.literals))
+#
 
 class Heuristic(metaclass=ABCMeta):
     FIRST_ERROR = False
 
     def __init__(self, initial_state: 'State'):
-        self.box_goal_positions = {}
-        self.agent_goal_positions = {}
-        for lit in initial_state.goal_literals:
-            if isinstance(lit, AgentAt):
-                self.agent_goal_positions[lit.agt] = lit.loc
-            elif isinstance(lit, BoxAt):
-                self.box_goal_positions[lit.box] = lit.loc
-
+        self.box_goal_positions = extract_goal_positions(BoxAt, initial_state, 'box')
+        self.agent_goal_positions = extract_goal_positions(AgentAt, initial_state, 'agt')
         self.num_rows = len(Free.walls)
         self.num_cols = len(Free.walls[0])
 
     def h(self, state: 'State') -> 'int':
-        goal_count = 0
-
-        # Count agents not at their goal positions
-        for agent, goal_loc in self.agent_goal_positions.items():
-            agent_loc = None
-            for lit in state.literals:
-                if isinstance(lit, AgentAt) and lit.agt == agent:
-                    agent_loc = lit.loc
-            if agent_loc and agent_loc != goal_loc:
-                goal_count += 1
-
-        # Count boxes not at their goal positions
-        for box, goal_loc in self.box_goal_positions.items():
-            box_loc = None
-            for lit in state.literals:
-                if isinstance(lit, BoxAt) and lit.box == box:
-                    box_loc = lit.loc
-            if box_loc and box_loc != goal_loc:
-                goal_count += 1
-
-        return goal_count
+        return count_goals_not_met(state, self.agent_goal_positions, AgentAt, 'agt') + \
+            count_goals_not_met(state, self.box_goal_positions, BoxAt, 'box')
 
     @abstractmethod
     def f(self, state: "State") -> "int":
